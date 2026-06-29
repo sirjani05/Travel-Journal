eead1582afa8b80bc74109a9ecda112f4618b321
sirjani05 <keithjani05@gmail.com>
2026-06-29 09:52:44 +0200

    fourth commit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
-const menuBtn = document.querySelector("i");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener('click', (e) =>{
-  navLinks.classList.toggle('open');
+menuBtn.addEventListener("click", (e) =>{
+  navLinks.classList.toggle("open");
 
   const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
-})
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
 navLinks.addEventListener("click", (e) =>{
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
-})
+});
 
 
 const scrollRevealOption = {
